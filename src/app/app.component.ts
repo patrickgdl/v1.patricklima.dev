6477d8b9fafbb2be5bf72c5cbbcb7dd9e0f8324b
skidrou <patrickgoncalveslima@gmail.com>
2020-01-24 17:41:55 -0300

    list & grid article view + init light or dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
+import { Component, OnInit } from '@angular/core';
 import { IdleMonitorService } from '@scullyio/ng-lib';
-import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  bodyElement: HTMLElement;
+
   constructor(private idle: IdleMonitorService) {}
 
-  title = 'angular-theme-novela';
+  ngOnInit() {
+    this.bodyElement = document.getElementsByTagName('BODY')[0] as HTMLElement;
+    if (localStorage.getItem('isLight') === null) {
+      localStorage.setItem('isLight', 'true');
+    }
+
+    if (localStorage.getItem('isLight') === 'false') {
+      this.makeDark();
+    } else {
+      this.makeLight();
+    }
+  }
+
+  makeDark() {
+    this.bodyElement.style.setProperty('--primary', '#fff');
+    this.bodyElement.style.setProperty('--secondary', '#fff');
+    this.bodyElement.style.setProperty('--grey', '#73737D');
+    this.bodyElement.style.setProperty('--background-color', '#111216');
+    this.bodyElement.style.setProperty('--accent', '#E9DAAC');
+    this.bodyElement.style.setProperty('--hover', 'rgba(255, 255, 255, 0.07)');
+    this.bodyElement.style.setProperty('--gradient', 'linear-gradient(180deg, #111216 0%, rgba(66, 81, 98, 0.36) 100%)');
+    this.bodyElement.style.setProperty('--articleText', '#fff');
+    this.bodyElement.style.setProperty('--track', 'rgba(255, 255, 255, 0.3)');
+    this.bodyElement.style.setProperty('--progress', '#fff');
+    this.bodyElement.style.setProperty('--card', '#1D2128');
+    this.bodyElement.style.setProperty('--error', '#EE565B');
+    this.bodyElement.style.setProperty('--success', '#46B17B');
+    this.bodyElement.style.setProperty('--errorBackground', 'rgba(238, 86, 91, 0.1)');
+    this.bodyElement.style.setProperty('--horizontalRule', 'rgba(255, 255, 255, 0.15)');
+    this.bodyElement.style.setProperty('--inputBackground', 'rgba(255, 255, 255, 0.07)');
+    this.bodyElement.style.setProperty('--tooltip', '#000');
+  }
+
+  makeLight() {
+    this.bodyElement.style.setProperty('--primary', '');
+    this.bodyElement.style.setProperty('--secondary', '');
+    this.bodyElement.style.setProperty('--grey', '');
+    this.bodyElement.style.setProperty('--background-color', '');
+    this.bodyElement.style.setProperty('--accent', '');
+    this.bodyElement.style.setProperty('--hover', '');
+    this.bodyElement.style.setProperty('--gradient', '');
+    this.bodyElement.style.setProperty('--articleText', '');
+    this.bodyElement.style.setProperty('--track', '');
+    this.bodyElement.style.setProperty('--progress', '');
+    this.bodyElement.style.setProperty('--card', '');
+    this.bodyElement.style.setProperty('--error', '');
+    this.bodyElement.style.setProperty('--success', '');
+    this.bodyElement.style.setProperty('--errorBackground', '');
+    this.bodyElement.style.setProperty('--horizontalRule', '');
+    this.bodyElement.style.setProperty('--inputBackground', '');
+    this.bodyElement.style.setProperty('--tooltip', 'lightgrey');
+  }
 }
--- src/app/sections/articles/hero/hero.component.ts
+++ src/app/sections/articles/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HeroComponent implements OnInit {
+  isList = false;
+  listRemoveWidth: MediaQueryList;
+  listAddWidth: MediaQueryList;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    document.getElementById('rows').style.fill = 'var(--grey)';
+
+    this.listRemoveWidth = window.matchMedia('(max-width: 735px)');
+    this.listAddWidth = window.matchMedia('(min-width: 735px)');
   }
 
+  @HostListener('window:resize', ['$event'])
+  resize(event: any) {
+    this.adjustListAtMobile();
+  }
+
+  adjustListAtMobile() {
+    if (this.listRemoveWidth.matches) {
+      document.getElementById('articlesList').classList.remove('articles-list-container-alt');
+    } else if (this.listAddWidth.matches && this.isList) {
+      document.getElementById('articlesList').classList.add('articles-list-container-alt');
+    }
+  }
+
+  makeGrid() {
+    document.getElementById('articlesList').classList.remove('articles-list-container-alt');
+    document.getElementById('tiles').style.fill = 'var(--primary)';
+    document.getElementById('rows').style.fill = 'var(--grey)';
+    this.isList = false;
+  }
+
+  makeRows() {
+    document.getElementById('articlesList').classList.add('articles-list-container-alt');
+    document.getElementById('rows').style.fill = 'var(--primary)';
+    document.getElementById('tiles').style.fill = 'var(--grey)';
+    this.isList = true;
+  }
 }
