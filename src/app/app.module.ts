923f477d71341b79fa16d4d4a559345ec1c70a67
patrickgdl <patrickgdlima@gmail.com>
2020-02-26 17:43:18 -0300

    refactor(core): moving folders to match order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +7,7 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+  imports: [AppRoutingModule, CoreModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,16 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { FooterComponent } from '../components/footer/footer.component';
-import { HeaderComponent } from '../components/header/header.component';
-import { LinkComponent } from '../components/icons/link/link.component';
-import { LogoComponent } from '../components/icons/logo/logo.component';
-import { SharedModule } from './../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LinkComponent } from './link/link.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [HeaderComponent, LogoComponent, LinkComponent, FooterComponent],
   exports: [HeaderComponent, LogoComponent, LinkComponent, FooterComponent],
-  imports: [CommonModule, SharedModule, RouterModule]
+  imports: [BrowserModule, SharedModule, RouterModule]
 })
 export class CoreModule {}
