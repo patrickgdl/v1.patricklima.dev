25104b5f2f64222c5cf05569fdf7e077927bdedd
patrickgdl <patrickgdlima@gmail.com>
2020-06-10 23:41:52 -0300

    build(version): new build version

---
+++ scully.patricklima.config.js
@@ -0,0 +1,12 @@
+exports.config = {
+    projectRoot: './src/app',
+    outDir: './build',
+    routes: {
+        '/post/:slug': {
+            type: 'contentFolder',
+            slug: {
+                folder: './posts'
+            }
+        }
+    }
+};
